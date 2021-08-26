13c5cf0fe30f1124f527af1f3e698dd644824a0c
Hien-BT01 <hienbui25g@gmail.com>
2021-08-26 20:41:26 +0700

    Sign in form

--- src/js/form.js
+++ src/js/form.js
@@ -7,10 +7,15 @@ const loginPasswordField = document.querySelector("#password-in")
 const signUpEmailField = document.querySelector("#email_box");
 const signUpPasswordField = document.querySelector("#password_box");
 const signUpConfirmPasswordField = document.querySelector("#confirm_box");
+const signInEmail = document.querySelector("#email-in")
+const signInPassword = document.querySelector("#password-in")
 
 
 Cookies.set("isLog","false", { path: "/" })
-
+let accounts = [{
+    userName:"hienbui27g@gmail.com",
+    password:"123456789"
+}];
 
 function goNewAccount(){
     container.classList.add("switch-active"); 
@@ -25,19 +30,42 @@ function goAlreadyHaveAccount(){
 }
 function signUp(e){
     e.preventDefault();
-    container.classList.remove("switch-active");
+    if(signUpPasswordField.value !== signUpConfirmPasswordField.value){
+        alert("Confirmed password is different from original password")
+        return;
+    }
+    const userAccount = {
+        userName:signUpEmailField.value,
+        password:signUpPasswordField.value
+    }
+    const checkIsSameEmail = accounts.every(account => account.userName !== userAccount.userName);
+    if(checkIsSameEmail === true){
+        accounts = [...accounts,userAccount]
+        alert("Sign up sucessfully")
+        container.classList.remove("switch-active");
+    }else{
+        alert("This email has already existed")
+    }
     signUpEmailField.value = ""
     signUpPasswordField.value = ""
     signUpConfirmPasswordField.value = ""
 }
 function signIn(e){
-    if(loginEmailField.value !== "TestAccount@gmail.com" || loginPasswordField.value !== "H123456789!"){
+    const userAccount = {
+        userName:signInEmail.value,
+        password:signInPassword.value
+    }
+    const {userName, password} = userAccount;
+    const isValidAccount = accounts.some(account => account.userName === userName && account.password === password)
+    
+    if(isValidAccount === false){
         e.preventDefault();
         alert("Email account or Password is wrong")
         return;
     }
     Cookies.set("isLog","true")
 }
+
 
 
 
